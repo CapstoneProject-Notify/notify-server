ff14990639adc7d6e328cbaf729636158f6c6b8c
alenjb <alenjb@naver.com>
2024-04-27 17:15:39 +0900

    #1 [fix] 스크랩 저장소 스크랩 제거 기능 수정
    - 스크랩 저장소의 제거 기능은 파라미터로 스크랩을 받아서 제거하는 것이 아니라 사용자 정보, 공지사항 정보를 받아서 제거하기 때문에 이 부분을 수정함

--- src/main/java/com/example/notifyserver/scrap/repository/ScrapRepository.java
+++ src/main/java/com/example/notifyserver/scrap/repository/ScrapRepository.java
@@ -1,10 +1,14 @@
 package com.example.notifyserver.scrap.repository;
 
+import com.example.notifyserver.common.domain.NoticeType;
 import com.example.notifyserver.scrap.domain.Scrap;
+import feign.Param;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,12 +22,17 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     @Override
     Scrap save(@NotNull Scrap scrap);
 
+
     /**
      * 스크랩을 DB에서 제거한다.
-     * @param scrap 제거할 스크랩 객체
+     * @param userId 제거할 스크랩의 소유 회원
+     * @param noticeId 제거할 공지사항의 ID
+     * @param type 제거할 공지사항의 종류
      */
-    @Override
-    void delete(@NotNull Scrap scrap);
+
+    @Modifying
+    @Query("DELETE FROM Scrap s WHERE s.user.userId = :userId AND s.notice.noticeId = :noticeId AND s.notice.noticeType = :type")
+    void deleteByUserIdAndNoticeIdAndType(@Param("userId") Long userId, @Param("noticeId") Long noticeId, @Param("type") NoticeType type);
 
     /**
      * 페이지에 맞게 스크랩을 DB에서 가져온다.
