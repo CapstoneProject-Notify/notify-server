63d3f8b86ce8addd59fa191113f6897d98cbf397
alenjb <alenjb@naver.com>
2024-04-28 06:59:45 +0900

    #1 [feat] 스크랩 성공 상태코드 및 메시지 추가

--- src/main/java/com/example/notifyserver/common/exception/enums/SuccessCode.java
+++ src/main/java/com/example/notifyserver/common/exception/enums/SuccessCode.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
 
-    LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃 성공입니다.");
+    LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃 성공입니다."),
+    SAVE_SCRAP_SUCCESS(HttpStatus.OK, "스크랩 성공입니다."),
+    DELETE_SCRAP_SUCCESS(HttpStatus.OK, "스크랩 제거 성공입니다."),
+    GET_SCRAP_SUCCESS(HttpStatus.OK, "스크랩 조회 성공입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
