ae3f0e33daba9cb33e8723040a11f3749c944b20
alenjb <alenjb@naver.com>
2024-04-28 06:02:48 +0900

    #1 [fix] 스크랩 서비스 인터페이스 스크랩 조회 기능 수정
    - 반환 값에 페이지 정보를 추가하도록 수정함

--- src/main/java/com/example/notifyserver/scrap/service/ScrapService.java
+++ src/main/java/com/example/notifyserver/scrap/service/ScrapService.java
@@ -3,6 +3,7 @@ package com.example.notifyserver.scrap.service;
 import com.example.notifyserver.common.domain.Notice;
 import com.example.notifyserver.scrap.domain.Scrap;
 import com.example.notifyserver.user.domain.User;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,7 +28,7 @@ public interface ScrapService {
      * 스크랩 목록에서 해당 페이지의 스크랩을 조회한다.
      * @param userId 스크랩 조회를 요청한 유저의 ID
      * @param pageNum 조회 페이지 번호
-     * @return 페이지 번호에 해당하는 스크랩 리스트
+     * @return 페이지 번호에 해당하는 스크랩들
      */
-    List<Scrap> getScrap(long userId, long pageNum);
+    Page<Scrap> getScrap(long userId, long pageNum);
 }
