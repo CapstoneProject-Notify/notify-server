d6bf491bacf6cf010c2e10a5895609600b3101aa
alenjb <alenjb@naver.com>
2024-04-27 17:19:11 +0900

    #1 [test] 스크랩 제거기능 테스트 구현

--- src/test/java/com/example/notifyserver/scrap/service/ScrapServiceImplTest.java
+++ src/test/java/com/example/notifyserver/scrap/service/ScrapServiceImplTest.java
@@ -8,9 +8,13 @@ import com.example.notifyserver.user.domain.User;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
 @SpringBootTest
@@ -25,8 +29,22 @@ class ScrapServiceImplTest {
 
     @Test
     void doScrap() {
-
         //given
+        Bundle bundle = getBundle();
+        em.persist(bundle.user());
+        em.persist(bundle.notice());
+
+        //when
+        long findId = scrapService.doScrap(bundle.user(), bundle.notice());
+        Scrap scrappedNotice = scrapRepository.findById(findId).orElse(null);
+
+        //then
+        assert scrappedNotice != null;
+        assertThat(scrappedNotice.getScrapId()).isEqualTo(findId);
+    }
+
+    @NotNull
+    private static Bundle getBundle() {
         User user = User.builder()
                 .googleId("구글아이디")
                 .nickName("닉네임")
@@ -39,15 +57,26 @@ class ScrapServiceImplTest {
                 .noticeType(NoticeType.COM)
                 .noticeUrl("공지 url")
                 .build();
+        Bundle bundle = new Bundle(user, notice);
+        return bundle;
+    }
+
+    private record Bundle(User user, Notice notice) {
+    }
+
+    @Test
+    public void deleteScrap() throws Exception{
+        //given
+        Bundle bundle = getBundle();
+        em.persist(bundle.user());
+        em.persist(bundle.notice());
+        scrapService.doScrap(bundle.user, bundle.notice);
+        int size = scrapRepository.findAll().size();
 
         //when
-        em.persist(user);
-        em.persist(notice);
-        long findId = scrapService.doScrap(user, notice);
-        Scrap scrappedNotice = scrapRepository.findById(findId).orElse(null);
-
+        scrapService.deleteScrap(bundle.user, bundle.notice);
+        
         //then
-        assert scrappedNotice != null;
-        Assertions.assertThat(scrappedNotice.getScrapId()).isEqualTo(findId);
+        assertEquals(size-1, scrapRepository.findAll().size());
     }
 }
