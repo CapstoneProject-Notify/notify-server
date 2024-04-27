0811cf1f544533dda4a4a098a6ec995c017ec452
alenjb <alenjb@naver.com>
2024-04-27 16:37:46 +0900

    #1 [fix] 스크랩 저장소 @NotNull 추가
    - 스크랩 저장소의 저장과 삭제 기능의 파라미터에 @NotNull 어노테이션을 추가함

--- src/main/java/com/example/notifyserver/scrap/repository/ScrapRepository.java
+++ src/main/java/com/example/notifyserver/scrap/repository/ScrapRepository.java
@@ -1,6 +1,7 @@
 package com.example.notifyserver.scrap.repository;
 
 import com.example.notifyserver.scrap.domain.Scrap;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,18 +12,18 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 
     /**
      * 스크랩을 DB에 저장한다.
-     * @param scrap 스크렙 객체
+     * @param scrap 스크랩 객체
      * @return 저장한 스크랩 객체
      */
     @Override
-    Scrap save(Scrap scrap);
+    Scrap save(@NotNull Scrap scrap);
 
     /**
      * 스크랩을 DB에서 제거한다.
-     * @param entity 제거할 스크랩 객체
+     * @param scrap 제거할 스크랩 객체
      */
     @Override
-    void delete(Scrap entity);
+    void delete(@NotNull Scrap scrap);
 
     /**
      * 페이지에 맞게 스크랩을 DB에서 가져온다.
