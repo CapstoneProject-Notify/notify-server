d949058278c1eb308a367e5fde620b2cecbc8eaf
alenjb <alenjb@naver.com>
2024-04-28 12:50:19 +0900

    #1 [fix] 스크랩 저장소 트랜잭션 어노테이션 추가

--- src/main/java/com/example/notifyserver/scrap/repository/ScrapRepository.java
+++ src/main/java/com/example/notifyserver/scrap/repository/ScrapRepository.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
@@ -20,6 +21,7 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
      * @param scrap 스크랩 객체
      * @return 저장한 스크랩 객체
      */
+    @Transactional
     @Override
     Scrap save(@NotNull Scrap scrap);
 
@@ -28,12 +30,13 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
      * 스크랩을 DB에서 제거한다.
      * @param userId 제거할 스크랩의 소유 회원
      * @param noticeId 제거할 공지사항의 ID
-     * @param type 제거할 공지사항의 종류
+     * @param noticeType 제거할 공지사항의 종류
      */
 
+    @Transactional
     @Modifying
-    @Query("DELETE FROM Scrap s WHERE s.user.userId = :userId AND s.notice.noticeId = :noticeId AND s.notice.noticeType = :type")
-    void deleteByUserIdAndNoticeIdAndType(@Param("userId") Long userId, @Param("noticeId") Long noticeId, @Param("type") NoticeType type);
+    @Query("DELETE FROM Scrap s WHERE s.user.userId = :userId AND s.notice.noticeId = :noticeId AND s.notice.noticeType = :noticeType")
+    void deleteByUserIdAndNoticeIdAndType(@Param("userId") Long userId, @Param("noticeId") Long noticeId, @Param("noticeType") NoticeType noticeType);
 
     /**
      * 페이지에 맞게 스크랩을 DB에서 가져온다.
@@ -41,5 +44,6 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
      * @param pageable 페이지 정보
      * @return 보여줄 스크랩들 및 페이지 정보
      */
+    @Transactional
     Page<Scrap> findAllByUser(User user, Pageable pageable);
 }
