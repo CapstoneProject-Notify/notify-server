8552f60792bce2c3b86e5bc6e48b7f8d6b49d0db
alenjb <alenjb@naver.com>
2024-04-28 09:57:48 +0900

    #1 [feat] 각 공지사항 저장소 추가

---
+++ src/main/java/com/example/notifyserver/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.notifyserver.user.repository;
+
+import com.example.notifyserver.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
