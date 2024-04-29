cd737369ae6ed9ab634501ba1691d35b2807de3d
alenjb <alenjb@naver.com>
2024-04-29 12:16:07 +0900

    #1 [feat] 스크랩 조회 응답 객체 생성

--- src/main/java/com/example/notifyserver/scrap/controller/ScrapController.java
+++ src/main/java/com/example/notifyserver/scrap/controller/ScrapController.java
@@ -14,6 +14,7 @@ import com.example.notifyserver.common.repository.NoticeRepository;
 import com.example.notifyserver.scrap.domain.Scrap;
 import com.example.notifyserver.scrap.dto.DeleteScrapRequest;
 import com.example.notifyserver.scrap.dto.GetScrapsResponse;
+import com.example.notifyserver.scrap.dto.NoticeResponse;
 import com.example.notifyserver.scrap.dto.SaveScrapRequest;
 import com.example.notifyserver.scrap.service.ScrapService;
 import com.example.notifyserver.user.domain.User;
@@ -24,6 +25,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -78,4 +80,52 @@ public class ScrapController {
             return ErrorResponse.error(ErrorCode.NOT_FOUND_RESOURCE_EXCEPTION);
         }
     }
+
+    /**
+     * 스크랩을 조회
+     * @param googleId 스크랩을 조회할 사용자의 구글 아이디
+     * @param pageNum 스크랩을 조회할 페이지 숫자
+     * @return 성공 코드, 스크랩 정보 , 페이지 정보
+     */
+    @GetMapping
+    public Response getScraps(@RequestHeader("googleId") String googleId, @RequestParam("page") long pageNum){
+
+        User findUser = userRepository.findUserByGoogleId(googleId)
+                .orElseThrow(() -> new NotFoundUserException(ErrorCode.NOT_FOUND_USER_EXCEPTION));
+        PageRequest pageRequest = PageRequest.of((int) pageNum, (int) NoticeConstants.PAGE_SIZE);
+
+        try {
+            Page<Scrap> result = scrapService.getScrap(findUser.getUserId(), pageRequest);
+            List<Scrap> scrapList = result.getContent();
+            GetScrapsResponse response = GetScrapsResponse.builder()
+                    .totalPages(result.getTotalPages())
+                    .page(result.getNumber())
+                    .notices(scrapToNoticeResponse(scrapList))
+                    .build();
+            return SuccessResponse.success(SuccessCode.GET_SCRAP_SUCCESS,response);
+        }catch (Exception e) {
+            return ErrorResponse.error(ErrorCode.NOT_FOUND_RESOURCE_EXCEPTION);
+        }
+    }
+
+    /**
+     * DB에서 가져온 스크랩을 API 형식에 맞게 변환해주는 메서드
+     * @param scrapList DB에서 가져온 스크랩
+     * @return API 요구 형식
+     */
+    private static List<NoticeResponse> scrapToNoticeResponse(List<Scrap> scrapList) {
+        List<NoticeResponse> list = new ArrayList<>();
+        for(Scrap s : scrapList){
+            list.add(
+                    NoticeResponse.builder()
+                            .scrapId(s.getScrapId())
+                            .title(s.getNotice().getNoticeTitle())
+                            .url(s.getNotice().getNoticeUrl())
+                            .isScrapped(true)
+                            .build()
+            );
+        }
+        return list;
+    }
+
 }
