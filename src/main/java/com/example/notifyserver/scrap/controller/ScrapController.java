e8caa725676cc5dd9ef7e0cc6153c5cd87d2f1e8
alenjb <alenjb@naver.com>
2024-05-01 09:58:26 +0900

    [hotfix] 회원 기능 및 스크랩 기능 CORS 적용

--- src/main/java/com/example/notifyserver/scrap/controller/ScrapController.java
+++ src/main/java/com/example/notifyserver/scrap/controller/ScrapController.java
@@ -31,7 +31,6 @@ import java.util.List;
 @RequestMapping("/scrap")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "*", methods = RequestMethod.GET)
 public class ScrapController {
 
     private final ScrapService scrapService;
