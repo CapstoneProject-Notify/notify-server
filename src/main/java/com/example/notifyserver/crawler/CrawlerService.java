da12961cde2f6b4bc5bf28dc87da0b4c7cdcf413
alenjb <alenjb@naver.com>
2024-05-10 02:07:12 +0900

    [feat] 크롤러 서비스 인터페이스 생성 및 새 글 관련 메서드 추가

---
+++ src/main/java/com/example/notifyserver/crawler/CrawlerService.java
@@ -0,0 +1,32 @@
+package com.example.notifyserver.crawler;
+
+import com.example.notifyserver.common.domain.Notice;
+import com.example.notifyserver.common.domain.NoticeType;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public interface CrawlerService {
+
+    // 1. 새 글 확인 필요 메서드
+
+    /**
+     * 학교 사이트에 로그인 후 게시판 버튼을 클릭하여 게시판 페이지에 진입한다.
+     */
+    public void loginAndGoToComNoticePage();
+
+    /**
+     * DB에서 가장 최신의 글 2개의 제목과 날짜를 가져온다.
+     * @param noticeType 공지사항의 타입
+     * @return 제목과 날짜를 매핑한 객체
+     */
+    public Map<String, String> getLastTwoNotices(NoticeType noticeType);
+    
+    /**
+     * 새 글이 있는지 확인 후 새 글의 개수를 반환한다.
+     * @param noticeType 공지사항의 타입
+     * @return 새 글의 개수
+     */
+    public int getNewNoticeCount(NoticeType noticeType);
+}
