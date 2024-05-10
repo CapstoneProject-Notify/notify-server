40a48c2b3f28144b2edaa985f48026b05d32fbd4
alenjb <alenjb@naver.com>
2024-05-10 19:52:39 +0900

    [test] 로그인 테스트 버그 수정
    - application.yml에서 값을 읽어오지 못하는 오류를 수정함

--- src/test/java/com/example/notifyserver/crawler/service/CrawlerServiceImplTest.java
+++ src/test/java/com/example/notifyserver/crawler/service/CrawlerServiceImplTest.java
@@ -6,8 +6,7 @@ import com.example.notifyserver.common.domain.NoticeType;
 import com.example.notifyserver.common.repository.NoticeRepository;
 import com.example.notifyserver.crawler.dto.TitlesAndDates;
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -16,38 +15,42 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Random;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=classpath:application.yml")
 @Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CrawlerServiceImplTest {
     @Autowired
-    CrawlerService service;
+    CrawlerService crawlerService;
     @Autowired
     NoticeRepository noticeRepository;
     @Autowired
     EntityManager em;
 
-    // Headless 모드로 Chrome 실행
+    @Value("${skku.username}") String encodedUsername;
+    @Value("${skku.password}") String encodedPassword;
+
     ChromeOptions options = new ChromeOptions().addArguments("--headless");
-    WebDriver webDriver = new ChromeDriver(options);
 
     @Test
-    void loginAndGoToComNoticePage() {
-        //given
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
-        service.loginAndGoToComNoticePage(webDriver);
+    @Order(1)
+    public void loginAndGoToComNoticePage() {
 
+        //given
+        WebDriver webDriver = new ChromeDriver(options);
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+        crawlerService.loginAndGoToComNoticePage(webDriver,
+                new String(Base64.getDecoder().decode(encodedUsername)),
+                new String(Base64.getDecoder().decode(encodedPassword)));
         //when
         // 로그인을 해야지만 들어갈 수 있는 링크
         webDriver.get(NoticeConstants.BOARD_PAGE);
@@ -79,7 +82,7 @@ class CrawlerServiceImplTest {
         //when
         em.persist(notice1);
         em.persist(notice2);
-        String[][] lastTwoNotices = service.getLastTwoNotices(NoticeType.COM);
+        String[][] lastTwoNotices = crawlerService.getLastTwoNotices(NoticeType.COM);
 
         //then
         assertEquals(notice1.getNoticeTitle(), lastTwoNotices[1][0]);
@@ -89,21 +92,28 @@ class CrawlerServiceImplTest {
     }
 
     @Test
+    @Order(2)
     public void getNewNoticeCount() throws Exception{
+        WebDriver webDriver = new ChromeDriver(options);
         //given
-        service.loginAndGoToComNoticePage(webDriver);
-        TitlesAndDates titlesAndDates = service.getTitlesAndDates(webDriver);
+        crawlerService.loginAndGoToComNoticePage(webDriver,
+                new String(Base64.getDecoder().decode(encodedUsername)),
+                new String(Base64.getDecoder().decode(encodedPassword)));
+        TitlesAndDates titlesAndDates = crawlerService.getTitlesAndDates(webDriver);
         List<String> titles = titlesAndDates.titles();
         List<String> dates = titlesAndDates.dates();
 
         //when
         int randNum = new Random().nextInt(1, 14);
+
+        //더 최신 공지
         Notice notice1 = Notice.builder().
                 noticeTitle(titles.get(randNum)).
                 noticeDate(new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy", Locale.ENGLISH).parse(dates.get(randNum))).
                 noticeType(NoticeType.COM).
                 noticeUrl("url1").
                 build();
+        // 더 오래된 공지
         Notice notice2 = Notice.builder().
                 noticeTitle(titles.get(randNum+1)).
                 noticeDate(new SimpleDateFormat("E MMM dd HH:mm:ss z yyyy", Locale.ENGLISH).parse(dates.get(randNum+1))).
@@ -114,7 +124,7 @@ class CrawlerServiceImplTest {
         em.persist(notice2);
         em.persist(notice1);
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(3));
-        int newNoticeCount = service.getNewNoticeCount(NoticeType.COM, webDriver, service.getLastTwoNotices(NoticeType.COM));
+        int newNoticeCount = crawlerService.getNewNoticeCount(NoticeType.COM, webDriver, crawlerService.getLastTwoNotices(NoticeType.COM));
 
         webDriver.close();
         //then
