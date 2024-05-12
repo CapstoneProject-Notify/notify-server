f0f83ad7eecfcb6199ad31230d2aa0860dd2a1ee
alenjb <alenjb@naver.com>
2024-05-12 23:28:49 +0900

    #20 [fix] 크롤러 서비스 기능들의 반환 값 및 파라미터 수정

--- src/main/java/com/example/notifyserver/crawler/service/CrawlerService.java
+++ src/main/java/com/example/notifyserver/crawler/service/CrawlerService.java
@@ -1,10 +1,13 @@
 package com.example.notifyserver.crawler.service;
 
+import com.example.notifyserver.common.domain.Notice;
 import com.example.notifyserver.common.domain.NoticeType;
 import com.example.notifyserver.crawler.dto.TitlesAndDates;
 import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -19,36 +22,36 @@ public interface CrawlerService {
      * @param password 로그인 PW
      */
 
-    public void loginAndGoToComNoticePage(WebDriver driver, String username, String password);
+    WebDriver loginAndGoToComNoticePage(WebDriver driver, String username, String password);
 
     /**
      * DB에서 가장 최신의 글 2개의 제목과 날짜를 가져온다.
      * @param noticeType 공지사항의 타입
      * @return 제목과 날짜를 매핑한 객체
      */
-    public String [][] getLastTwoNotices(NoticeType noticeType);
+    String [][] getLastTwoNotices(NoticeType noticeType);
     
     /**
      * 새 글이 있는지 확인 후 새 글의 개수를 반환한다.
      * @param noticeType 공지사항의 타입
      * @return 새 글의 개수
      */
-    public int getNewNoticeCount(NoticeType noticeType, WebDriver driver, String [][] top2);
+    int getNewNoticeCount(NoticeType noticeType, WebDriver driver, String[][] top2) throws InterruptedException, ParseException;
 
     /**
-     * 공지사항 페이지에서 공지사항의 제목과 날짜를 가져온다.
+     * 공지사항 페이지에서 공통 공지사항의 제목과 날짜를 가져온다.
      * @param driver 크롬 드라이버
      * @return 공지사항 제목과 날짜 리스트
      */
-    public TitlesAndDates getTitlesAndDates(WebDriver driver);
+    TitlesAndDates getTitlesAndDatesOfComNoticeFromPageNum(WebDriver driver, int pageNum) throws ParseException;
 
     /**
-     * 새 글의 개수를 찾는다.
+     * 공통 공지사항의 새 글의 개수를 찾는다.
      * @param top2 DB에 저장된 가장 최근 게시물 2개
      * @param driver 크롬 드라이버
      * @return 새 글의 개수
      */
-    public int findNewNoticeOrder(String[][] top2, WebDriver driver);
+    int findNewNoticeOrder(String[][] top2, WebDriver driver) throws InterruptedException, ParseException;
 
     /**
      * 새 글의 유무를 체크한다.
@@ -57,13 +60,18 @@ public interface CrawlerService {
      * @param dates 페이지에서 가져온 날짜들
      * @return 새 글의 유무
      */
-    public boolean newNoticeCheck(String[][] top2, List<String> titles, List<String> dates);
+    boolean newComNoticeCheck(String[][] top2, List<String> titles, List<Date> dates);
 
     /**
-     * 날짜 형식을 바꾸는 메서드
-     * @param dateString 페이지에서 가져온 날짜 텍스트
-     * @return DB에 저장할 날짜 형식을 문자열로 바꾼 값
+     * 해당 페이지 번호에서 공지사항들을 가져옵니다.
+     * @param username 로그인 ID
+     * @param password 로그인 PW
+     * @param pageNum 가져올 페이지 번호
+     * @param driver 크롬 드라이버
+     * @return 해당 페이지 번호의 공지사항들 리스트
+     * @throws InterruptedException
+     * @throws ParseException
      */
-    public String dateFormating(String dateString);
+    List<Notice> getNewNoticesByPageNum(String username, String password, int pageNum, WebDriver driver) throws InterruptedException, ParseException;
 
 }
