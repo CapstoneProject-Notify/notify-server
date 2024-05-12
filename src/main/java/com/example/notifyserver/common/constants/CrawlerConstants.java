3d91a2a626a7ffdde4138066e4a58640503ef782
alenjb <alenjb@naver.com>
2024-05-12 23:32:21 +0900

    #20 [feat] 공통 공지사항 관련 상수 추가

---
+++ src/main/java/com/example/notifyserver/common/constants/CrawlerConstants.java
@@ -0,0 +1,7 @@
+package com.example.notifyserver.common.constants;
+
+public final class CrawlerConstants {
+    // 한 페이지에 크롤링 해오는 공통 공지사항 개수
+    public static final int CRAWLING_COM_NOTICE_SIZE_PER_PAGE = 15;
+
+}
