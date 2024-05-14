0c0c4803691ba29c4e2cb2c09843c46beec1040e
alenjb <alenjb@naver.com>
2024-05-15 01:10:21 +0900

    #20 [fix] 공지사항 타입 enum에 페이지당 공지사항 개수 추가

--- src/main/java/com/example/notifyserver/common/domain/NoticeType.java
+++ src/main/java/com/example/notifyserver/common/domain/NoticeType.java
@@ -1,23 +1,31 @@
 package com.example.notifyserver.common.domain;
 
+import com.example.notifyserver.common.constants.CrawlerConstants;
+
 public enum NoticeType {
     /**
      * 공지 사항 타입
      */
-    COM("com"),    // 공통 공지사항
-    BUS("bus"),    // 경영학과 공지사항
-    COS("cos"),    // 유학동양학과 공지사항
-    AAI("aai"),    // 인공지능융합학과 공지사항
-    ESM("esm");     // 시스템경영공학과 공지사항
+    COM("com", CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE),    // 공통 공지사항
+    BUS("bus", CrawlerConstants.CRAWLING_BUS_NOTICE_SIZE_PER_PAGE),    // 경영학과 공지사항
+    COS("cos", CrawlerConstants.CRAWLING_COS_NOTICE_SIZE_PER_PAGE),    // 유학동양학과 공지사항
+    AAI("aai", CrawlerConstants.CRAWLING_AAI_NOTICE_SIZE_PER_PAGE),    // 인공지능융합학과 공지사항
+    ESM("esm", CrawlerConstants.CRAWLING_ESM_NOTICE_SIZE_PER_PAGE);     // 시스템경영공학과 공지사항
 
     private final String type;
+    private final int noticeSizePerPage; // 한 페이지에 크롤링 해오는 공지사항 개수
 
-    NoticeType(String type) {
+    NoticeType(String type, int noticeSizePerPage) {
         this.type = type;
+        this.noticeSizePerPage = noticeSizePerPage;
     }
 
     public String getType() {
         return type;
+    }
+
+    public int getNoticeSizePerPage() {
+        return noticeSizePerPage;
     }
 
     /**
