09939ed9abdbc46b5e016f79d7618a748f12200c
alenjb <alenjb@naver.com>
2024-05-15 01:22:38 +0900

    #20 [refactor] 크롤링 서비스 코드 정리 및 확장
    - 공통 공지사항에만 적용되던 일부 기능들을 모든 공통사항에 적용할 수 있도록 확장함

--- src/main/java/com/example/notifyserver/crawler/service/CrawlerService.java
+++ src/main/java/com/example/notifyserver/crawler/service/CrawlerService.java
@@ -13,6 +13,45 @@ import java.util.List;
 @Service
 public interface CrawlerService {
 
+    /** ================ 모든 공지사항 관련 기능 (공통, 학과 모두 사용 가능) ================ **/
+
+    /**
+     * DB에서 가장 최신의 글 2개의 제목과 날짜를 가져온다.
+     * @param noticeType 공지사항의 타입
+     * @return 제목과 날짜를 매핑한 객체
+     */
+    String[][] getLastTwoNotices(NoticeType noticeType);
+
+    /**
+     * 새 글의 개수를 반환한다.
+     * @param noticeType 공지사항의 타입
+     * @param driver 크롬 드라이버
+     * @param top2 DB에 저장된 가장 최근 게시물 2개
+     * @return
+     */
+    int getNewNoticeCount(NoticeType noticeType, WebDriver driver, String[][] top2) throws InterruptedException, ParseException;
+
+    /**
+     * 공지사항의 새 글의 개수를 찾는다.
+     * @param top2 DB에 저장된 가장 최근 게시물 2개
+     * @param driver 크롬 드라이버
+     * @param noticeType 공지사항의 타입
+     * @return 새 글의 개수
+     * @throws InterruptedException
+     * @throws ParseException
+     */
+    int findNewNoticeOrder(String[][] top2, WebDriver driver, NoticeType noticeType) throws InterruptedException, ParseException;
+
+    /**
+     * 새 공지사항의 유무를 체크한다.
+     * @param top2 DB에 저장된 가장 최근 게시물 2개
+     * @param titles 페이지에서 가져온 제목들
+     * @param dates 페이지에서 가져온 날짜들
+     * @return 새 글의 유무
+     */
+    boolean newNoticeCheck(String[][] top2, List<String> titles, List<Date> dates);
+
+
     /** ================ 공통 공지사항 관련 기능 ================ **/
 
     /**
@@ -25,43 +64,11 @@ public interface CrawlerService {
     WebDriver loginAndGoToComNoticePage(WebDriver driver, String username, String password);
 
     /**
-     * DB에서 가장 최신의 글 2개의 제목과 날짜를 가져온다.
-     *
-     * @param noticeType 공지사항의 타입
-     * @return 제목과 날짜를 매핑한 객체
-     */
-    String[][] getLastTwoNotices(NoticeType noticeType);
-    
-    /**
-     * 새 글이 있는지 확인 후 새 글의 개수를 반환한다.
-     * @param noticeType 공지사항의 타입
-     * @return 새 글의 개수
-     */
-    int getNewComNoticeCount(NoticeType noticeType, WebDriver driver, String[][] top2) throws InterruptedException, ParseException;
-
-    /**
      * 공지사항 페이지에서 공통 공지사항의 제목과 날짜를 가져온다.
      * @param driver 크롬 드라이버
      * @return 공지사항 제목과 날짜 리스트
      */
     TitlesAndDates getTitlesAndDatesOfComNoticeFromPageNum(WebDriver driver, int pageNum) throws ParseException;
-
-    /**
-     * 공통 공지사항의 새 글의 개수를 찾는다.
-     * @param top2 DB에 저장된 가장 최근 게시물 2개
-     * @param driver 크롬 드라이버
-     * @return 새 글의 개수
-     */
-    int findNewNoticeOrder(String[][] top2, WebDriver driver) throws InterruptedException, ParseException;
-
-    /**
-     * 새 공통 공지사항의 유무를 체크한다.
-     * @param top2 DB에 저장된 가장 최근 게시물 2개
-     * @param titles 페이지에서 가져온 제목들
-     * @param dates 페이지에서 가져온 날짜들
-     * @return 새 글의 유무
-     */
-    boolean newComNoticeCheck(String[][] top2, List<String> titles, List<Date> dates);
 
     /**
      * 해당 페이지 번호에서 공지사항들을 가져온다.
@@ -109,18 +116,6 @@ public interface CrawlerService {
     TitlesAndDates getTitlesAndDatesOfMajorNoticeFromPageNum(WebDriver driver, int pageNum, NoticeType noticeType) throws ParseException;
 
     /**
-     * 학과 공지사항 새 글이 있는지 확인 후 새 글의 개수를 반환한다. 위에 있음
-     */
-
-    /**
-     * 학과 공지사항 새 글의 유무를 체크한다.
-     */
-
-    /**
-     * 학과 공지사항 새 글의 개수를 반환한다.
-     */
-
-    /**
      * 해당 페이지 번호에서 학과 공지사항들을 가져온다.
      */
 
@@ -130,7 +125,7 @@ public interface CrawlerService {
      * @return Date 객체로 변환한 학과 공지사항의 날짜
      * @throws ParseException
      */
-    public Date parseMajorNoticeDateAndFormatting(String dateString) throws ParseException;
+    Date parseMajorNoticeDateAndFormatting(String dateString) throws ParseException;
 
     /**
      * 새 학과 공지사항들을 DB에 저장한다.
--- src/main/java/com/example/notifyserver/crawler/service/CrawlerServiceImpl.java
+++ src/main/java/com/example/notifyserver/crawler/service/CrawlerServiceImpl.java
@@ -11,10 +11,8 @@ import com.example.notifyserver.common.repository.NoticeRepository;
 import com.example.notifyserver.crawler.dto.TitlesAndDates;
 import com.example.notifyserver.crawler.repository.CrawlerRepository;
 import org.jetbrains.annotations.NotNull;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -37,6 +35,138 @@ public class CrawlerServiceImpl implements CrawlerService{
     @Autowired
     NoticeRepository noticeRepository;
 
+
+    /** ================ 모든 공지사항 관련 기능 (공통, 학과 모두 사용 가능) ================ **/
+
+    /**
+     * DB에서 가장 최신의 글 2개의 제목과 날짜를 가져온다.
+     * @param noticeType 공지사항의 타입
+     * @return 제목과 날짜를 매핑한 객체
+     */
+    @Override
+    public String[][] getLastTwoNotices(NoticeType noticeType) {
+        List<Notice> top2 = crawlerRepository.findTop2ByOrderByCreatedAtDesc(noticeType);
+        String [][] result = new String[2][2]; // 각 행의 0번째 인덱스에는 제목이 1번쨰 인덱스에는 날짜가 들어있음. 0번쨰 행이 더 최신 글임
+
+        for (int i=0; i<2; i++) {
+            String noticeTitle = top2.get(i).getNoticeTitle();
+            Date noticeDate = top2.get(i).getNoticeDate();
+            result[i][0] = noticeTitle;
+            result[i][1] = new Date(noticeDate.getTime()).toString();
+        }
+        return result;
+    }
+
+    /**
+     * 새 글의 개수를 반환한다.
+     * @param noticeType 공지사항의 타입
+     * @param driver 크롬 드라이버
+     * @param top2 DB에 저장된 가장 최근 게시물 2개
+     * @return
+     */
+    @Override
+    public int getNewNoticeCount(NoticeType noticeType, WebDriver driver, String [][] top2) throws InterruptedException, ParseException {
+        // 페이지에서 제목 목록과 날짜 목록을 가져오기
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        TitlesAndDates titlesAndDates = null;
+        // 공통 공지사항인 경우 첫 페이지의 글들을 가져오기
+        if(noticeType == NoticeType.COM) titlesAndDates = getTitlesAndDatesOfComNoticeFromPageNum(driver, 1);
+            // 학과 공지사항인 경우 첫 페이지의 글들을 가져오기
+        else getTitlesAndDatesOfMajorNoticeFromPageNum(driver, 1, noticeType);
+
+        boolean hasNewNotice = newNoticeCheck(top2, titlesAndDates.titles(), titlesAndDates.dates());
+        // 새 글이 없는 경우 0을 반환
+        if(!hasNewNotice) return 0;
+            // 새 글이 있는 경우 새 글이 몇개 있는지 반환
+        else return findNewNoticeOrder(top2, driver, noticeType);
+    }
+
+    /**
+     * 공지사항의 새 글의 개수를 찾는다.
+     * @param top2 DB에 저장된 가장 최근 게시물 2개
+     * @param driver 크롬 드라이버
+     * @param noticeType 공지사항의 타입
+     * @return 새 글의 개수
+     * @throws InterruptedException
+     * @throws ParseException
+     */
+    @Override
+    public int findNewNoticeOrder(String[][] top2, WebDriver driver, NoticeType noticeType) throws InterruptedException, ParseException {
+        String firstTitle = top2[0][0]; // DB의 첫번째(제일 최근) 게시물의 제목
+        String firstDate = top2[0][1]; // DB의 첫번째(제일 최근) 게시물의 날짜
+        String secondTitle = top2[1][0]; // DB의 두번째 게시물의 제목
+        String secondDate = top2[1][1]; // DB의 두번째 게시물의 날짜
+
+        // 1페이지부터 10페이지까지 반복하면서
+        for(int i=1; i<=10; i++){
+            // 해당 페이지로 이동
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement pageButton;
+            if(noticeType == NoticeType.COM){ // 공통 공지사항인 경우
+                pageButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.dhx_page:nth-of-type(" + i + ")")));
+            }else { // 학과 공지사항인 경우
+                pageButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".paging-wrap > li:nth-of-type(" + i + ")")));
+            }
+            pageButton.click();
+
+
+            // 클릭이 정상적으로 적용되도록 0.5초 대기
+            Thread.sleep(500);
+
+            TitlesAndDates titlesAndDates = null;
+            // 페이지에 있는 공통 공지사항들의 제목과 날짜 가져오기
+            // 공통 공지사항인 경우
+            if(noticeType == NoticeType.COM) titlesAndDates = getTitlesAndDatesOfComNoticeFromPageNum(driver, i);
+                // 학과 공지사항인 경우
+            else titlesAndDates = getTitlesAndDatesOfMajorNoticeFromPageNum(driver, i, noticeType);
+
+
+
+            // Date 형식인 titlesAndDates.dates()와 firstDate, secondDate를 비교하기 위해 날짜 객체를 문자로 바꿔서 리스트 생성
+            List<String> StringDates = new ArrayList<>();
+            for (Date date : titlesAndDates.dates()) {
+                StringDates.add(date.toString());
+            }
+
+            // DB의 첫번째 게시물의 인덱스를 찾으면 새글의 개수를 반환
+            if(titlesAndDates.titles().contains(firstTitle)
+                    && titlesAndDates.titles().indexOf(firstTitle) == StringDates.indexOf(firstDate))
+                return (i-1) * noticeType.getNoticeSizePerPage() + titlesAndDates.titles().indexOf(firstTitle);
+
+                // DB의 두번째 게시물의 인덱스를 찾으면 첫번째 게시물이 수정이나 삭제되었다고 여기고 두번째 게시글 이후를 새글로 간주 후 개수를 반환
+            else if(titlesAndDates.titles().contains(secondTitle)
+                    && titlesAndDates.titles().indexOf(secondTitle) == StringDates.indexOf(secondDate))
+                return (i-1) * noticeType.getNoticeSizePerPage() + titlesAndDates.titles().indexOf(secondTitle);
+        }
+        // 10페이지까지 반복해도 새 게시물이 없는 경우 예외 발생
+        throw new NotFoundException(ErrorCode.NOT_FOUND_RESOURCE_EXCEPTION);
+    }
+
+    /**
+     * 새 공지사항의 유무를 체크한다.
+     * @param top2 DB에 저장된 가장 최근 게시물 2개
+     * @param titles 페이지에서 가져온 제목들
+     * @param dates 페이지에서 가져온 날짜들
+     * @return 새 글의 유무
+     */
+    @Override
+    public boolean newNoticeCheck(String[][] top2, List<String> titles, List<Date> dates) {
+        String firstTitle = top2[0][0]; // 가장 최신 글의 제목
+        String firstDate = top2[0][1]; // 가장 최신 글의 날짜
+        String secondTitle = top2[1][0]; // 두번째 최신 글의 제목
+        String secondDate = top2[1][1]; // 두번째 최신 글의 제목
+        boolean hasFirst = false; // 페이지에 가장 최신글의 존재 유무
+        boolean hasSecond = false; // 페이지에 두번째 최신글의 존재 유무
+
+        // 가장 최신의 글 2개가 DB의 가장 최신의 글 두개와 일치하는지 확인
+        for(int i=0; i<2; i++){
+            if(titles.get(i).equals(firstTitle) && dates.get(i).toString().equals(firstDate)) hasFirst = true;
+            else if(titles.get(i).equals(secondTitle) && dates.get(i).toString().equals(secondDate)) hasSecond = true;
+        }
+        return !(hasFirst && hasSecond);
+    }
+
+
     /** ================ 공통 공지사항 관련 기능 ================ **/
 
     /**
@@ -52,7 +182,7 @@ public class CrawlerServiceImpl implements CrawlerService{
         try {
             // 웹 페이지 열기
             driver.get(CrawlerConstants.LOGIN_PAGE);
-        } catch (org.openqa.selenium.NoSuchSessionException e) {
+        } catch (NoSuchSessionException e) {
             // 세션 다시 시작
             driver.quit();
             // Headless 모드로 Chrome 실행
@@ -81,46 +211,6 @@ public class CrawlerServiceImpl implements CrawlerService{
         boardButton.click();
 
         return driver;
-    }
-
-    /**
-     * DB에서 가장 최신의 글 2개의 제목과 날짜를 가져온다.
-     *
-     * @param noticeType 공지사항의 타입
-     * @return 제목과 날짜를 매핑한 객체
-     */
-    @Override
-    public String[][] getLastTwoNotices(NoticeType noticeType) {
-        List<Notice> top2 = crawlerRepository.findTop2ByOrderByCreatedAtDesc(noticeType);
-        String [][] result = new String[2][2]; // 각 행의 0번째 인덱스에는 제목이 1번쨰 인덱스에는 날짜가 들어있음. 0번쨰 행이 더 최신 글임
-
-        for (int i=0; i<2; i++) {
-            String noticeTitle = top2.get(i).getNoticeTitle();
-            Date noticeDate = top2.get(i).getNoticeDate();
-            result[i][0] = noticeTitle;
-            result[i][1] = new Date(noticeDate.getTime()).toString();
-        }
-        return result;
-    }
-
-    /**
-     * 새 글의 개수를 반환한다.
-     * @param noticeType 공지사항의 타입
-     * @param driver 크롬 드라이버
-     * @param top2 DB에 저장된 가장 최근 게시물 2개
-     * @return
-     */
-    @Override
-    public int getNewComNoticeCount(NoticeType noticeType, WebDriver driver, String [][] top2) throws InterruptedException, ParseException {
-        // 페이지에서 제목 목록과 날짜 목록을 가져오기
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        TitlesAndDates titlesAndDates = getTitlesAndDatesOfComNoticeFromPageNum(driver, 1);
-
-        boolean hasNewNotice = newComNoticeCheck(top2, titlesAndDates.titles(), titlesAndDates.dates());
-        // 새 글이 없는 경우 0을 반환
-        if(!hasNewNotice) {return 0;}
-        // 새 글이 있는 경우 새 글이 몇개 있는지 반환
-        else return findNewNoticeOrder(top2, driver);
     }
 
     /**
@@ -168,78 +258,7 @@ public class CrawlerServiceImpl implements CrawlerService{
     }
 
     /**
-     * 공통 공지사항의 새 글의 개수를 찾는다.
-     * @param top2 DB에 저장된 가장 최근 게시물 2개
-     * @param driver 크롬 드라이버
-     * @return 새 글의 개수
-     */
-    @Override
-    public int findNewNoticeOrder(String[][] top2, WebDriver driver) throws InterruptedException, ParseException {
-        String firstTitle = top2[0][0]; // DB의 첫번째(제일 최근) 게시물의 제목
-        String firstDate = top2[0][1]; // DB의 첫번째(제일 최근) 게시물의 날짜
-        String secondTitle = top2[1][0]; // DB의 두번째 게시물의 제목
-        String secondDate = top2[1][1]; // DB의 두번째 게시물의 날짜
-
-        // 1페이지부터 10페이지까지 반복하면서
-        for(int i=1; i<=10; i++){
-            // 해당 페이지로 이동
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            WebElement pageButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.dhx_page:nth-of-type(" + i + ")")));
-            pageButton.click();
-
-            // 클릭이 정상적으로 적용되도록 0.5초 대기
-            Thread.sleep(500);
-
-            // 페이지에 있는 공통 공지사항들의 제목과 날짜 가져오기
-            TitlesAndDates titlesAndDates = getTitlesAndDatesOfComNoticeFromPageNum(driver, i);
-
-
-            // Date 형식인 titlesAndDates.dates()와 firstDate, secondDate를 비교하기 위해 날짜 객체를 문자로 바꿔서 리스트 생성
-            List<String> StringDates = new ArrayList<>();
-            for (Date date : titlesAndDates.dates()) {
-                StringDates.add(date.toString());
-            }
-
-            // DB의 첫번째 게시물의 인덱스를 찾으면 새글의 개수를 반환
-            if(titlesAndDates.titles().contains(firstTitle)
-                    && titlesAndDates.titles().indexOf(firstTitle) == StringDates.indexOf(firstDate))
-                return (i-1) * CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE + titlesAndDates.titles().indexOf(firstTitle);
-
-                // DB의 두번째 게시물의 인덱스를 찾으면 첫번째 게시물이 수정이나 삭제되었다고 여기고 두번째 게시글 이후를 새글로 간주 후 개수를 반환
-            else if(titlesAndDates.titles().contains(secondTitle)
-                    && titlesAndDates.titles().indexOf(secondTitle) == StringDates.indexOf(secondDate))
-                return (i-1) * CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE + titlesAndDates.titles().indexOf(secondTitle);
-        }
-        // 10페이지까지 반복해도 새 게시물이 없는 경우 예외 발생
-        throw new NotFoundException(ErrorCode.NOT_FOUND_RESOURCE_EXCEPTION);
-    }
-
-    /**
-     * 새 공통 공지사항의 유무를 체크한다.
-     * @param top2 DB에 저장된 가장 최근 게시물 2개
-     * @param titles 페이지에서 가져온 제목들
-     * @param dates 페이지에서 가져온 날짜들
-     * @return 새 글의 유무
-     */
-    @Override
-    public boolean newComNoticeCheck(String[][] top2, List<String> titles, List<Date> dates) {
-        String firstTitle = top2[0][0]; // 가장 최신 글의 제목
-        String firstDate = top2[0][1]; // 가장 최신 글의 날짜
-        String secondTitle = top2[1][0]; // 두번째 최신 글의 제목
-        String secondDate = top2[1][1]; // 두번째 최신 글의 제목
-        boolean hasFirst = false; // 페이지에 가장 최신글의 존재 유무
-        boolean hasSecond = false; // 페이지에 두번째 최신글의 존재 유무
-
-        // 가장 최신의 글 2개가 DB의 가장 최신의 글 두개와 일치하는지 확인
-        for(int i=0; i<2; i++){
-            if(titles.get(i).equals(firstTitle) && dates.get(i).toString().equals(firstDate)) hasFirst = true;
-            else if(titles.get(i).equals(secondTitle) && dates.get(i).toString().equals(secondDate)) hasSecond = true;
-        }
-        return !(hasFirst && hasSecond);
-    }
-
-    /**
-     * 해당 페이지 번호에서 공지사항들을 가져온다.
+     * 해당 페이지 번호에서 공통 공지사항들을 가져온다.
      * @param username 로그인 ID
      * @param password 로그인 PW
      * @param pageNum 가져올 페이지 번호
@@ -392,7 +411,7 @@ public class CrawlerServiceImpl implements CrawlerService{
             driver.close();
             // 아이디 입력 요소가 존재하면 로그인되지 않은 상태로 간주하여 false 반환
             return false;
-        } catch (org.openqa.selenium.NoSuchElementException e) {
+        } catch (NoSuchElementException e) {
             driver.close();
             // 아이디 입력 요소가 없으면 로그인된 상태로 간주하여 true 반환
             return true;
@@ -448,7 +467,7 @@ public class CrawlerServiceImpl implements CrawlerService{
         WebElement pageButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".paging-wrap > li:nth-of-type(" + pageNum + ")")));
         pageButton.click();
 
-        // 클래스 이름이 .ev_dhx_terrace 인 모든 요소 가져오기
+        // 클래스 이름이 .board-list-content-wrap 인 모든 요소 가져오기
         List<WebElement> dynamicElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".board-list-content-wrap")));
 
         // 타이틀과 날짜를 저장할 리스트 생성
