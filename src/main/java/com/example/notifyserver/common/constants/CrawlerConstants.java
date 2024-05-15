1ccf99eda1359892a70f869706ed985bc2a96633
alenjb <alenjb@naver.com>
2024-05-15 14:41:17 +0900

    #20 [fix] 크롤링 상수 및 공지 타입 객체 수정
    - 크롤링할 페이지의 공지사항 개수를 수정함
    - 공지사항 타입에서 공지사항 페이지 url과 한 페이지당 공지 수를 확인할 수 있도록 수정함

--- src/main/java/com/example/notifyserver/common/constants/CrawlerConstants.java
+++ src/main/java/com/example/notifyserver/common/constants/CrawlerConstants.java
@@ -12,7 +12,7 @@ public final class CrawlerConstants {
     /** ================ 학과 공지사항 관련 상수 ================ **/
     /* 1. 경영학과 */
     // 한 페이지에 크롤링 해오는 경영학과 공지사항 개수
-    public static final int CRAWLING_BUS_NOTICE_SIZE_PER_PAGE = 15;
+    public static final int CRAWLING_BUS_NOTICE_SIZE_PER_PAGE = 10;
     // 경영학과 공지사항 게시판 페이지 url
     public static final String BUS_NOTICE_BOARD_PAGE = "https://biz.skku.edu/bizskk/notice.do";
 
@@ -26,12 +26,12 @@ public final class CrawlerConstants {
     // 한 페이지에 크롤링 해오는 인공지능융합학과 공지사항 개수
     public static final int CRAWLING_AAI_NOTICE_SIZE_PER_PAGE = 10;
     // 인공지능융합학과 공지사항 게시판 페이지 url
-    public static final String AAI_NOTICE_BOARD_PAGE = "https://xai.skku.edu/skkuaai/total_notice.do";
+    public static final String AAI_NOTICE_BOARD_PAGE = "https://xai.skku.edu/skkuaai/notice.do";
 
-    /* 4. 시스탬경영공학과 */
-    // 한 페이지에 크롤링 해오는 시스탬경영공학과 공지사항 개수
-    public static final int CRAWLING_ESM_NOTICE_SIZE_PER_PAGE = 13;
-    // 시스탬경영공학과 공지사항 게시판 페이지 url
+    /* 4. 시스템경영공학과 */
+    // 한 페이지에 크롤링 해오는 시스템경영공학과 공지사항 개수
+    public static final int CRAWLING_ESM_NOTICE_SIZE_PER_PAGE = 10;
+    // 시스템경영공학과 공지사항 게시판 페이지 url
     public static final String ESM_NOTICE_BOARD_PAGE = "https://sme.skku.edu/iesys/notice.do";
 
 
--- src/main/java/com/example/notifyserver/common/domain/NoticeType.java
+++ src/main/java/com/example/notifyserver/common/domain/NoticeType.java
@@ -6,18 +6,20 @@ public enum NoticeType {
     /**
      * 공지 사항 타입
      */
-    COM("com", CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE),    // 공통 공지사항
-    BUS("bus", CrawlerConstants.CRAWLING_BUS_NOTICE_SIZE_PER_PAGE),    // 경영학과 공지사항
-    COS("cos", CrawlerConstants.CRAWLING_COS_NOTICE_SIZE_PER_PAGE),    // 유학동양학과 공지사항
-    AAI("aai", CrawlerConstants.CRAWLING_AAI_NOTICE_SIZE_PER_PAGE),    // 인공지능융합학과 공지사항
-    ESM("esm", CrawlerConstants.CRAWLING_ESM_NOTICE_SIZE_PER_PAGE);     // 시스템경영공학과 공지사항
+    COM("com", CrawlerConstants.CRAWLING_COM_NOTICE_SIZE_PER_PAGE, CrawlerConstants.COM_NOTICE_BOARD_PAGE),    // 공통 공지사항
+    BUS("bus", CrawlerConstants.CRAWLING_BUS_NOTICE_SIZE_PER_PAGE, CrawlerConstants.BUS_NOTICE_BOARD_PAGE),    // 경영학과 공지사항
+    COS("cos", CrawlerConstants.CRAWLING_COS_NOTICE_SIZE_PER_PAGE, CrawlerConstants.COS_NOTICE_BOARD_PAGE),    // 유학동양학과 공지사항
+    AAI("aai", CrawlerConstants.CRAWLING_AAI_NOTICE_SIZE_PER_PAGE, CrawlerConstants.AAI_NOTICE_BOARD_PAGE),    // 인공지능융합학과 공지사항
+    ESM("esm", CrawlerConstants.CRAWLING_ESM_NOTICE_SIZE_PER_PAGE, CrawlerConstants.ESM_NOTICE_BOARD_PAGE);     // 시스템경영공학과 공지사항
 
     private final String type;
     private final int noticeSizePerPage; // 한 페이지에 크롤링 해오는 공지사항 개수
+    private final String boardUrl; // 공지사항 게시판 페이지 url
 
-    NoticeType(String type, int noticeSizePerPage) {
+    NoticeType(String type, int noticeSizePerPage, String boardUrl) {
         this.type = type;
         this.noticeSizePerPage = noticeSizePerPage;
+        this.boardUrl = boardUrl;
     }
 
     public String getType() {
@@ -26,6 +28,10 @@ public enum NoticeType {
 
     public int getNoticeSizePerPage() {
         return noticeSizePerPage;
+    }
+
+    public String getBoardUrl() {
+        return boardUrl;
     }
 
     /**
