a53a4e2ea045bbc2bee6057d605b50c9f9508f00
pkl0912 <pklpkl0912@gmail.com>
2024-05-23 17:18:42 +0900

    #37 [refactor] session 삭제

--- src/main/java/com/example/notifyserver/user/controller/UserController.java
+++ src/main/java/com/example/notifyserver/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.notifyserver.user.controller;
 
-import com.example.notifyserver.common.domain.Notice;
 import com.example.notifyserver.common.dto.ApiResponse;
 import com.example.notifyserver.common.dto.ErrorResponse;
 import com.example.notifyserver.common.dto.SuccessNonDataResponse;
@@ -12,8 +11,6 @@ import com.example.notifyserver.user.dto.request.RegisterRequest;
 import com.example.notifyserver.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,9 +21,9 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public ApiResponse userLogin(@RequestBody LoginRequest request, HttpSession session) {
+    public ApiResponse userLogin(@RequestBody LoginRequest request) {
         try {
-            userService.userLogin(request, session);
+            userService.userLogin(request);
             return SuccessNonDataResponse.success(SuccessCode.LOGIN_SUCCESS);
         } catch (NotFoundUserException e) {
             return ErrorResponse.error(ErrorCode.USER_NOT_FOUND_EXCEPTION);
@@ -57,17 +54,6 @@ public class UserController {
             return ErrorResponse.error(ErrorCode.USER_NOT_FOUND_EXCEPTION);
         } catch (Exception e) {
             return ErrorResponse.error(ErrorCode.INTERNAL_SERVER_EXCEPTION);
-        }
-    }
-
-    @PostMapping("/send")
-    public ResponseEntity<String> createNotice(@RequestBody Notice notice) {
-        try {
-            userService.findAndSendEmail(notice);
-            return ResponseEntity.ok("Notice created and emails sent successfully.");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the notice.");
         }
     }
 }
