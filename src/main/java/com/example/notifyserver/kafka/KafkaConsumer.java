c17edeab23e84aaf03176d002128d2b74fffb1c2
alenjb <alenjb@naver.com>
2024-06-02 22:00:03 +0900

    #52 [fix] 메시지 consumer 메서드 수정
    - 역직렬화된 공지 리스트를 다루도록 수정함

--- src/main/java/com/example/notifyserver/kafka/KafkaConsumer.java
+++ src/main/java/com/example/notifyserver/kafka/KafkaConsumer.java
@@ -1,16 +1,33 @@
 package com.example.notifyserver.kafka;
 
+import com.example.notifyserver.notice.domain.Notice;
+import com.example.notifyserver.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class KafkaConsumer {
+    @Autowired
+    UserService userService;
 
     @KafkaListener(topics = "notify-crawler-topic", groupId = "notify")
-    public void listen(String message) {
-        log.info(message + "from Kafka");
-        log.warn("===================="+message + "from Kafka ====================");
+    public void consume(List<Notice> notices) {
+        if (notices == null || notices.isEmpty()) {
+            // 비어있는 noticeList가 오는 경우에 대한 처리
+            log.info("================ 새롭게 크롤링 된 게시물이 없습니다. ================");
+            return;
+        }
+
+        // Kafka 메시지를 역직렬화한 List<Notice> 객체를 사용하여 원하는 작업을 수행
+        for (Notice notice : notices) {
+            log.info("================ 새롭게 크롤링 된 게시물 제목: ================" + notice.getNoticeTitle());
+//            userService.findAndSendEmail(notice);
+        }
+
     }
 }
